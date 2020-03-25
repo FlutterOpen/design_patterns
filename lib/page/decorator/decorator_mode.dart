437ae157d615ef2984fbd3b08a93116a90086b32
聂彬 <niebin@sh.hupu.com>
2020-03-25 11:11:49 +0800

    fix(): 修改装饰模式

--- lib/page/decorator/decorator_mode.dart
+++ lib/page/decorator/decorator_mode.dart
@@ -3,48 +3,53 @@ import 'package:flutter/cupertino.dart';
 /// Created by NieBin on 2020-03-24
 /// Github: https://github.com/nb312
 /// Email: niebin312@gmail.com
+///
+
 abstract class Building {
-  final IPaint paint;
-
-  Building(this.paint);
-
   String decorate();
 }
 
-abstract class IPaint {
-  String decoration();
+class Paint implements Building {
+  final Building building;
+
+  Paint(this.building);
+
+  @override
+  String decorate() {
+    return "1.${building.decorate()}";
+  }
 }
 
 ///商业建筑
 class BusinessBuilding extends Building {
-  BusinessBuilding(IPaint paint) : super(paint);
-
   @override
   String decorate() {
-    return "装修商业楼\n${paint.decoration()}";
+    return "装修商业楼";
   }
 }
 
 ///居民楼
 class PersonBuilding extends Building {
-  PersonBuilding(IPaint paint) : super(paint);
+  @override
+  String decorate() {
+    return "装修居民楼";
+  }
+}
+
+class RedPaint extends Paint {
+  RedPaint(Building building) : super(building);
 
   @override
   String decorate() {
-    return "装修居民楼\n${paint.decoration()}";
+    return "${super.decorate()}\n 2.粉刷为红色";
   }
 }
 
-class RedPaint extends IPaint {
-  @override
-  String decoration() {
-    return "粉刷为红色";
-  }
-}
+class GreenPaint extends Paint {
+  GreenPaint(Building building) : super(building);
 
-class GreenPaint extends IPaint {
   @override
-  String decoration() {
-    return "粉刷为绿色";
+  String decorate() {
+    return "${super.decorate()}\n 2.粉刷为绿色";
   }
 }
